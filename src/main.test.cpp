bf8276b677bc8641cadeccc43f5f91c3f3a0ad11
Branislav Goga <brano.goga@centrum.sk>
2022-12-03 00:51:25 +0100

    Test shortest path

--- src/main.test.cpp
+++ src/main.test.cpp
@@ -1,9 +1,72 @@
 #include <gtest/gtest.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+#include "graph.h"
+
+TEST(MainTest, shortest_path_1) {
+    WeightedDiGraph graph(4);
+    add_edge(0, 1, 2.0f, graph);
+    add_edge(0, 2, 3.0f, graph);
+    add_edge(1, 2, 1.0f, graph);
+    add_edge(0, 3, 1.0f, graph);
+    add_edge(3, 2, 1.0f, graph);
+
+    Vertex start = vertex(0, graph);
+    Vertex target = vertex(2, graph);
+    auto path = shortest_path(graph, start, target);
+
+    ASSERT_EQ(3, path.size());
+    ASSERT_EQ(0, path[0]);
+    ASSERT_EQ(3, path[1]);
+    ASSERT_EQ(2, path[2]);
 }
+
+TEST(MainTest, shortest_path_2) {
+    WeightedDiGraph graph(4);
+    add_edge(0, 1, 1.0f, graph);
+    add_edge(0, 2, 3.0f, graph);
+    add_edge(1, 2, 1.0f, graph);
+    add_edge(0, 3, 2.0f, graph);
+    add_edge(3, 2, 1.0f, graph);
+
+    Vertex start = vertex(0, graph);
+    Vertex target = vertex(2, graph);
+    auto path = shortest_path(graph, start, target);
+
+    ASSERT_EQ(3, path.size());
+    ASSERT_EQ(0, path[0]);
+    ASSERT_EQ(1, path[1]);
+    ASSERT_EQ(2, path[2]);
+}
+
+TEST(MainTest, shortest_path_3) {
+    WeightedDiGraph graph(4);
+    add_edge(0, 1, 1.0f, graph);
+    add_edge(0, 2, 1.0f, graph);
+    add_edge(1, 2, 1.0f, graph);
+    add_edge(0, 3, 1.0f, graph);
+    add_edge(3, 2, 1.0f, graph);
+
+    Vertex start = vertex(0, graph);
+    Vertex target = vertex(2, graph);
+    auto path = shortest_path(graph, start, target);
+
+    ASSERT_EQ(2, path.size());
+    ASSERT_EQ(0, path[0]);
+    ASSERT_EQ(2, path[1]);
+}
+
+// TODO: Infinite loop.
+//TEST(MainTest, shortest_path_not_connected) {
+//    WeightedDiGraph graph(4);
+//    add_edge(0, 1, 1.0f, graph);
+//    add_edge(3, 2, 1.0f, graph);
+//
+//    Vertex start = vertex(0, graph);
+//    Vertex target = vertex(2, graph);
+//    auto path = shortest_path(graph, start, target);
+//
+//    ASSERT_EQ(0, path.size());
+//}
+
+// TODO: Negative edge
+// TODO: Negative cycle
