0ff0623ca519b03b2568bcf0c6fb63a1509c43b5
Branislav Goga <brano.goga@centrum.sk>
2022-12-02 22:55:04 +0100

    Add Boost Graph

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # Path finding
 Comparison of different algorithms for single &amp; multi agent path finding (_with application to real-world warehouse autonomous robot movement_)
 
-## Build
+## How to run
 
-## Run
+### Prerequisities
+1. [CMake](https://cmake.org/)
+2. [Connan package manager](https://conan.io/)
+
+### Build and run
+
+Linux:
+'
+mkdir .conan && cd .conan
+conan install .. -s compiler.libcxx=libstdc++11
+cd ..
+cmake .
+make
+chmod +x path-finder
+./path-finder
+'
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <sstream>
 
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphviz.hpp>
+
 #include "version.h"
 
 std::string getVersion() {
@@ -10,7 +13,28 @@ std::string getVersion() {
     return out.str();
 }
 
+void print_graph() {
+    typedef boost::adjacency_list< boost::listS, boost::vecS, boost::directedS > digraph;
+
+    // instantiate a digraph object with 8 vertices
+    digraph g(8);
+
+    // add some edges
+    add_edge(0, 1, g);
+    add_edge(1, 5, g);
+    add_edge(5, 6, g);
+    add_edge(2, 3, g);
+    add_edge(2, 4, g);
+    add_edge(3, 5, g);
+    add_edge(4, 5, g);
+    add_edge(5, 7, g);
+
+    // represent graph in DOT format and send to cout
+    write_graphviz(std::cout, g);
+}
+
 int main() {
     std::cout << "Hello Path Finding" << getVersion() << "!" << std::endl;
+    print_graph();
     return 0;
 }
