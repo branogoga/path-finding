f5222e868efe55e6f5b3ca4c4bfd9b91ae1b837a
Brano Goga <brano.goga@centrum.sk>
2024-05-25 22:27:06 +0200

    fix color names

--- src/color.cpp
+++ src/color.cpp
@@ -9,35 +9,10 @@
 // orange1:orange2:orange3:orange4:
 // orangered:pink:purple:violet:magenta:maroon:gray
 std::vector<std::string> colors{
-    "red1",
-    "green1",
-    "blue1",
-    "yellow1",
-    "orange1",
-    "pink"
-    "gray20",
-    "red4",
-    "green4",
-    "blue4",
-    "yellow4",
-    "orange4",
-    "purple"
-    "gray60",
-    "red2",
-    "green2",
-    "blue2",
-    "yellow2",
-    "orange2",
-    "violet"
-    "gray40",
-    "red3",
-    "green3",
-    "blue3",
-    "yellow3",
-    "orange3",
-    "magenta"
-    "gray80",
-    "maroon"};
+    "red1",    "green1",  "blue1",  "yellow1", "orange1", "pink",    "gray20",  "red4",    "green4",  "blue4",
+    "yellow4", "orange4", "purple", "gray60",  "red2",    "green2",  "blue2",   "yellow2", "orange2", "violet",
+    "gray40",  "red3",    "green3", "blue3",   "yellow3", "orange3", "magenta", "gray80",  "maroon",
+};
 
 std::string getGraphVizColor(unsigned index)
 {
